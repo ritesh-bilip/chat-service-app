e154f27acbaae12a9c44c42c85cb58a7ffe1405c
ritesh-bilip <dasritesh537@gmail.com>
2026-04-02 21:47:45 +0530

    fix  connect

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -1,21 +1,8 @@
-"""
-ASGI config for djangochat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 import django
-# from channels.routing import get_default_application
-from channels.security.websocket import AllowedHostsOriginValidator
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-# from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 django.setup()
@@ -23,12 +10,10 @@ django.setup()
 import chat.routing
 
 application = ProtocolTypeRouter({
-  "http":get_asgi_application(),
-  "websocket":AllowedHostsOriginValidator(
-    AuthMiddlewareStack(
-       URLRouter(
-        chat.routing.websocket_urlpatterns
-    )
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
-  ),
 })

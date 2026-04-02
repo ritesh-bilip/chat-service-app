849825ca41e44823a8287bcce078ac6ec8d81954
ritesh-bilip <dasritesh537@gmail.com>
2026-04-02 17:34:43 +0530

    fix issue

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$f*@#0fsa!n4m(+s*5jfz!4j(nro&w3=q)4_!-rz1be8$tlfu7')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
@@ -55,8 +55,9 @@ ASGI_APPLICATION = "djangochat.asgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        engine='django.db.backends.postgresql',
     )
 }
 
@@ -79,7 +80,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://red-d775d9haae7s73bm3ri0:6379')],
         },
     },
 }
@@ -100,6 +101,6 @@ REST_FRAMEWORK = {
 
 # CORS — allow your auth service to talk to chat service
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get('AUTH_SERVICE_URL', 'http://localhost:8000'),
+    os.environ.get('AUTH_SERVICE_URL', 'https://dasri-auth-service.onrender.com'),
 ]
 CORS_ALLOW_ALL_ORIGINS = True  # simplify for now
